176e054c43084344b37e64d82b0ad1dabf814c7a
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-03-11 12:02:14 +1100

    Next round of chaos; some cleanup.

--- src/tex_from_char.c
+++ src/tex_from_char.c
@@ -4,177 +4,6 @@
 #include <string.h>
 #include "tex/tex.h"
 
-#if 0
-tex_char_t tex_from_char(const char **s, uint32_t c) {
-    assert(s != NULL);
-
-    switch (c) {
-
-        /* Standard LaTeX special characters */
-        case L'\\':
-            *s = "{\\letterbackslash}";
-            return TEX_SEQUENCE;
-        case L'&':  return "{\\&}";
-        case L'%':  return "{\\%}";
-        case L'$':  return "{\\$}";
-        case L'#':  return "{\\#}";
-        case L'_':  return "{\\letterunderscore}";
-        case L'{':  return "{\\letteropenbrace}";
-        case L'}':  return "{\\letterclosebrace}";
-        case L'~':  return "{\\lettertilde}";
-        case L'^':  return "{\\letterhat}";
-
-        /* https://en.wikibooks.org/wiki/LaTeX/Special_Characters:
-         *
-         * > Less than < and greater than >[edit]
-         * >   The two symbols '<' and '>' are actually ASCII characters, but
-         * >   you may have noticed that they will print '¡' and '¿'
-         * >   respectively. This is a font encoding issue. If you want them to
-         * >   print their real symbol, you will have to use another font
-         * >   encoding such as T1, loaded with the fontenc package. See Fonts
-         * >   for more details on font encoding.
-         * >
-         * >   Alternatively, they can be printed with dedicated commands:
-         * >
-         * >     \textless
-         * >     \textgreater
-         *
-         */
-        case L'<':  return "{\\textless}";
-        case L'>':  return "{\\textgreater}";
-
-        /* Accented letters */
-        case L'à':  return "{\\`a}";   /* grave */
-        case L'á':  return "{\\'a}";   /* acute */
-        case L'â':  return "{\\^a}";   /* circumflex */
-        case L'ä':  return "{\\\"a}";  /* umlaut */
-        case L'a̋':  return "{\\H a}";  /* double acute */
-        case L'ã':  return "{\\~a}";   /* tilde */
-        case L'a̧':  return "{\\c a}";  /* cedilla */
-        case L'ā':  return "{\\=a}";   /* macron */
-        case L'a̱':  return "{\\b a}";  /* macron below */
-        case L'ȧ':  return "{\\.a}";   /* overdot */
-        case L'ă':  return "{\\u a}";  /* breve */
-        case L'ǎ':  return "{\\v a}";  /* caron */
-        case L'a͡':  return "{\\t a}";  /* tie */
-        case L'ạ':  return "{\\d a}";  /* underdot */
-        case L'å':  return "{\\r a}";  /* overring */
-        case L'ą':  return "{\\k a}";  /* ogonek */
-        case L'é':  return "{\\'e}";
-        case L'ő':  return "{\\H o}";
-        case L'ο̩':  return "{\\b o}";
-        case L'ş':  return "{\\c s}";
-
-        /* Superscript and subscript */
-        case L'₀':  return "\\textsubscript{0}";
-        case L'₁':  return "\\textsubscript{1}";
-        case L'₂':  return "\\textsubscript{2}";
-        case L'₃':  return "\\textsubscript{3}";
-        case L'₄':  return "\\textsubscript{4}";
-        case L'₅':  return "\\textsubscript{5}";
-        case L'₆':  return "\\textsubscript{6}";
-        case L'₇':  return "\\textsubscript{7}";
-        case L'₈':  return "\\textsubscript{8}";
-        case L'₉':  return "\\textsubscript{9}";
-        case L'ᴬ':  return "\\textsuperscript{A}";
-        case L'ᴮ':  return "\\textsuperscript{B}";
-        case L'ᴰ':  return "\\textsuperscript{D}";
-        case L'ᴱ':  return "\\textsuperscript{E}";
-        case L'ᴳ':  return "\\textsuperscript{G}";
-        case L'ᴴ':  return "\\textsuperscript{H}";
-        case L'ᴵ':  return "\\textsuperscript{I}";
-        case L'ᴶ':  return "\\textsuperscript{J}";
-        case L'ᴷ':  return "\\textsuperscript{K}";
-        case L'ᴸ':  return "\\textsuperscript{L}";
-        case L'ᴹ':  return "\\textsuperscript{M}";
-        case L'ᴺ':  return "\\textsuperscript{N}";
-        case L'ᴼ':  return "\\textsuperscript{O}";
-        case L'ᴾ':  return "\\textsuperscript{P}";
-        case L'ᴿ':  return "\\textsuperscript{R}";
-        case L'ᵀ':  return "\\textsuperscript{T}";
-        case L'ᵁ':  return "\\textsuperscript{U}";
-        case L'ⱽ':  return "\\textsuperscript{V}";
-        case L'ᵂ':  return "\\textsuperscript{W}";
-#if 0
-        /* small caps; what even are they? */
-        case L'ᶦ':  ???
-        case L'ᶫ':  ???
-        case L'ᶰ':  ???
-        case L'ᶸ':  ???                   
-#endif
-        case L'ᵃ':  return "\\textsuperscript{a}";
-        case L'ᵇ':  return "\\textsuperscript{b}";
-        case L'ᶜ':  return "\\textsuperscript{c}";
-        case L'ᵈ':  return "\\textsuperscript{d}";
-        case L'ᵉ':  return "\\textsuperscript{e}";
-        case L'ᶠ':  return "\\textsuperscript{f}";
-        case L'ᵍ':  return "\\textsuperscript{g}";
-        case L'ʰ':  return "\\textsuperscript{h}";
-        case L'ⁱ':  return "\\textsuperscript{i}";
-        case L'ʲ':  return "\\textsuperscript{j}";
-        case L'ᵏ':  return "\\textsuperscript{k}";
-        case L'ˡ':  return "\\textsuperscript{l}";
-        case L'ᵐ':  return "\\textsuperscript{m}";
-        case L'ⁿ':  return "\\textsuperscript{n}";
-        case L'ᵒ':  return "\\textsuperscript{o}";
-        case L'ᵖ':  return "\\textsuperscript{p}";
-        case L'ʳ':  return "\\textsuperscript{r}";
-        case L'ˢ':  return "\\textsuperscript{s}";
-        case L'ᵗ':  return "\\textsuperscript{t}";
-        case L'ᵘ':  return "\\textsuperscript{u}";
-        case L'ᵛ':  return "\\textsuperscript{v}";
-        case L'ʷ':  return "\\textsuperscript{w}";
-        case L'ˣ':  return "\\textsuperscript{x}";
-        case L'ʸ':  return "\\textsuperscript{y}";
-        case L'ᶻ':  return "\\textsuperscript{z}";
-        case L'ₐ':  return "\\textsubscript{a}";
-        case L'ₑ':  return "\\textsubscript{e}";
-        case L'ₕ':  return "\\textsubscript{h}";
-        case L'ᵢ':  return "\\textsubscript{i}";
-        case L'ⱼ':  return "\\textsubscript{j}";
-        case L'ₖ':  return "\\textsubscript{k}";
-        case L'ₗ':  return "\\textsubscript{l}";
-        case L'ₘ':  return "\\textsubscript{m}";
-        case L'ₙ':  return "\\textsubscript{n}";
-        case L'ₒ':  return "\\textsubscript{o}";
-        case L'ₚ':  return "\\textsubscript{p}";
-        case L'ᵣ':  return "\\textsubscript{r}";
-        case L'ₛ':  return "\\textsubscript{s}";
-        case L'ₜ':  return "\\textsubscript{t}";
-        case L'ᵤ':  return "\\textsubscript{u}";
-        case L'ᵥ':  return "\\textsubscript{v}";
-        case L'ₓ':  return "\\textsubscript{x}";
-
-        /* Greek */
-        case L'τ':  return "$\\tau$";
-        /* XXX: the rest of Greek */
-
-        /* Greek superscripts */
-        case L'ᵝ':  return "\\textsuperscript{$\\beta$}"; 
-        /*ᵞ   ᵟ   ᵋ           ᶿ   ᶥ                                           ᶹ   ᵠ   ᵡ       
-        Subscript minuscule     ᵦ   ᵧ                                                       ᵨ               ᵩ   ᵪ   
-        ᶛ ᶜ ᶝ ᶞ ᶟ ᶠ ᶡ ᶢ ᶣ ᶤ ᶥ ᶦ ᶧ ᶨ ᶩ ᶪ ᶫ ᶬ ᶭ ᶮ ᶯ ᶰ ᶱ ᶲ ᶳ ᶴ ᶵ ᶶ ᶷ ᶸ ᶹ ᶺ ᶻ ᶼ ᶽ ᶾ ᶿ.*/
-
-        /* German, Scandinavian */
-        case L'ß':  return "{\\ss}";
-        case L'æ':  return "{\\ae}";
-        case L'Æ':  return "{\\AE}";
-        case L'ł':  return "{\\l}";
-        case L'ø':  return "{\\o}";
-
-        /* Other misc symbols */
-        case L'§':  return "{\\S}";
-        case L'—':  return "{\\textemdash}";
-        case L'¶':  return "{\\P}";
-
-    }
-
-    assert(c <= 127 && "unhandled extended character");
-
-    return NULL;
-}
-#endif
-
 tex_char_t tex_from_char(char *s, uint32_t c) {
     assert(s != NULL);
 
@@ -250,16 +79,16 @@ tex_char_t tex_from_char(char *s, uint32_t c) {
         case L'¬':                 SEQ_TC("{\\textlnot}");
         case 0x00ad:               SEQ("\\-"); /* soft hyphen */
         case L'®':                 SEQ("{\\textregistered}");
-        case L'¯':                 return TEX_UNSUPPORTED;
+        case L'¯':                 SEQ("{\\= }");
         case L'°':                 SEQ_TC("{\\textdegree}");
         case L'±':                 SEQ("$\\pm$");
         case L'²':                 SEQ("\\textsuperscript{2}");
         case L'³':                 SEQ("\\textsuperscript{3}");
-        case L'´':                 return TEX_UNSUPPORTED;
+        case L'´':                 SEQ("{\\' }");
         case L'µ':                 SEQ("$\\mu$");
         case L'¶':                 SEQ("{\\P}");
         case L'·':                 SEQ("\\textperiodcentered}");
-        case L'¸':                 return TEX_UNSUPPORTED;
+        case L'¸':                 SEQ("{\\c }");
         case L'¹':                 SEQ("\\textsuperscript{1}");
         case L'º':                 SEQ("{\\textordmasculine}");
         case L'»':                 SEQ_T1("{\\guillemotright}");
@@ -348,6 +177,18 @@ tex_char_t tex_from_char(char *s, uint32_t c) {
         case L'Ď':                 SEQ("{\\v D}");
         case L'ď':                 SEQ("{\\v d}");
 
+        case L'Ł':                 SEQ("{\\L}");
+        case L'ł':                 SEQ("{\\l}");
+
+        case L'ˆ':                 ACC("{\\^");
+        case L'ˇ':                 ACC("{\\v ");
+
+        case L'ˉ':                 ACC("{\\=");
+        case L'ˊ':                 ACC("{\\'");
+        case L'ˋ':                 ACC("{\\`");
+
+        case L'ˍ':                 ACC("{\\b ");
+
 
         case 0x0300:               ACC("{\\`");
         case 0x0301:               ACC("{\\'");
@@ -367,6 +208,64 @@ tex_char_t tex_from_char(char *s, uint32_t c) {
         /* XXX */
         case 0x0331:               ACC("{\\b ");
         /* XXX */
+        case 0x0361:               ACC("{\\t ");
+
+        /* Greek */
+        case L'Α':                 SEQ("$A$");
+        case L'Β':                 SEQ("$B$");
+        case L'Γ':                 SEQ("$\\Gamma$");
+        case L'Δ':                 SEQ("$\\Delta$");
+        case L'Ε':                 SEQ("$E$");
+        case L'Ζ':                 SEQ("$Z$");
+        case L'Η':                 SEQ("$H$");
+        case L'Θ':                 SEQ("$\\Theta$");
+        case L'Ι':                 SEQ("$I$");
+        case L'Κ':                 SEQ("$K$");
+        case L'Λ':                 SEQ("$\\Lambda$");
+        case L'Μ':                 SEQ("$M$");
+        case L'Ν':                 SEQ("$N$");
+        case L'Ξ':                 SEQ("$\\Xi$");
+        case L'Ο':                 SEQ("$O$");
+        case L'Π':                 SEQ("$\\Pi$");
+        case L'Ρ':                 SEQ("$P$");
+        case 0x03a2:               return TEX_INVALID;
+        case L'Σ':                 SEQ("$\\Sigma$");
+        case L'Τ':                 SEQ("$T$");
+        case L'Υ':                 SEQ("$Y$");
+        case L'Φ':                 SEQ("$\\Phi$");
+        case L'Χ':                 SEQ("$X$");
+        case L'Ψ':                 SEQ("$\\Psi$");
+        case L'Ω':                 SEQ("$\\Omega$");
+        /* XXX */
+        case L'α':                 SEQ("$\\alpha$");
+        case L'β':                 SEQ("$\\beta$");
+        case L'γ':                 SEQ("$\\gamma$");
+        case L'δ':                 SEQ("$\\delta$");
+        case L'ε':                 SEQ("$\\varepsilon$");
+        case L'ζ':                 SEQ("$\\zeta$");
+        case L'η':                 SEQ("$\\eta$");
+        case L'θ':                 SEQ("$\\theta$");
+        case L'ι':                 SEQ("$\\iota$");
+        case L'κ':                 SEQ("$\\kappa$");
+        case L'λ':                 SEQ("$\\lambda$");
+        case L'μ':                 SEQ("$\\mu$");
+        case L'ν':                 SEQ("$\\nu$");
+        case L'ξ':                 SEQ("$\\xi$");
+        case L'ο':                 SEQ("$o$");
+        case L'π':                 SEQ("$\\pi$");
+        case L'ρ':                 SEQ("$\\rho$");
+        case L'ς':                 SEQ("$\\varsigma$");
+        case L'σ':                 SEQ("$\\sigma$");
+        case L'τ':                 SEQ("$\\tau$");
+        case L'υ':                 SEQ("$\\upsilon$");
+        case L'φ':                 SEQ("$\\phi$");
+        case L'χ':                 SEQ("$\\chi$");
+        case L'ψ':                 SEQ("$\\psi$");
+        case L'ω':                 SEQ("$\\omega$");
+
+        case L'—':                 SEQ("{\\textemdash}");
+
+        /* XXX */
         case L'⁰':                 SEQ("\\textsuperscript{0}");
         case L'ⁱ':                 SEQ("\\textsuperscript{i}");
         case 0x2072 ... 0x2073:    return TEX_INVALID;
@@ -376,7 +275,12 @@ tex_char_t tex_from_char(char *s, uint32_t c) {
         case L'⁷':                 SEQ("\\textsuperscript{7}");
         case L'⁸':                 SEQ("\\textsuperscript{8}");
         case L'⁹':                 SEQ("\\textsuperscript{9}");
-        /* XXX */
+        case L'⁺':                 SEQ("\\textsuperscript{+}");
+        case L'⁻':                 SEQ("\\textsuperscript{-}");
+        case L'⁼':                 SEQ("\\textsuperscript{=}");
+        case L'⁽':                 SEQ("\\textsuperscript{(}");
+        case L'⁾':                 SEQ("\\textsuperscript{)}");
+        case L'ⁿ':                 SEQ("\\textsuperscript{n}");
         case L'₀':                 SEQ("\\textsubscript{0}");
         case L'₁':                 SEQ("\\textsubscript{1}");
         case L'₂':                 SEQ("\\textsubscript{2}");
@@ -387,13 +291,26 @@ tex_char_t tex_from_char(char *s, uint32_t c) {
         case L'₇':                 SEQ("\\textsubscript{7}");
         case L'₈':                 SEQ("\\textsubscript{8}");
         case L'₉':                 SEQ("\\textsubscript{9}");
+        case L'₊':                 SEQ("\\textsubscript{+}");
+        case L'₋':                 SEQ("\\textsubscript{-}");
+        case L'₌':                 SEQ("\\textsubscript{=}");
+        case L'₍':                 SEQ("\\textsubscript{(}");
+        case L'₎':                 SEQ("\\textsubscript{)}");
+        case 0x208f:               return TEX_INVALID;
+        /* XXX */
+        case 0x209d ... 0x209f:    return TEX_INVALID;
 
+        /* UTF-8 characters are a maximum of 21 bits */
+        case 1 << 21 ... UINT32_MAX:
+            return TEX_INVALID;
+
+        default:
+            return TEX_UNSUPPORTED;
+            
 #undef SEQ
 #undef SEQ_T1
 #undef SEQ_TC
 #undef ACC
 
     }
-
-    return TEX_UNSUPPORTED;
 }
