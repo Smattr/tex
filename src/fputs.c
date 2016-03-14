8f481875e462f9f157ca4c8ef1f74b03c78e4cc0
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-03-14 14:41:48 +1100

    Turn string translation into a state machine; handle math mode.

--- src/fputs.c
+++ src/fputs.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <ctype.h>
 #include "internal.h"
 #include <stdbool.h>
 #include <stdio.h>
@@ -31,7 +32,20 @@ int utf8totex_fputs(const char *s, bool bibtex, FILE *f, utf8totex_char_t *error
         } \
     } while (0)
 
+#define PUTC(c) \
+    do { \
+        if (fputc((c), f) == EOF) { \
+            ERR(EOF); \
+        } \
+    } while (0)
+
     unsigned brace_depth = 0;
+    enum {
+        IDLE,
+        MACRO,
+        BRACED,
+        MATH,
+    } state = IDLE;
 
     uint32_t c;
     int length;
@@ -41,74 +55,134 @@ int utf8totex_fputs(const char *s, bool bibtex, FILE *f, utf8totex_char_t *error
         if (length == -1)
             ERR(INVALID);
 
-        if (bibtex) {
-            if (c == L'{' || brace_depth > 0) {
+        switch (state) {
+
+            case IDLE: {
+                if (bibtex) {
+                    if (c == L'\\') {
+                        FLUSH_LOOKAHEAD();
+                        PUTC('\\');
+                        state = MACRO;
+                        break;
+                    } else if (c == L'{') {
+                        FLUSH_LOOKAHEAD();
+                        PUTC('{');
+                        state = BRACED;
+                        assert(brace_depth == 0);
+                        brace_depth = 1;
+                        break;
+                    } else if (c == L'$') {
+                        FLUSH_LOOKAHEAD();
+                        PUTC('$');
+                        state = MATH;
+                        break;
+                    }
+                }
+
+                const char *t;
+                utf8totex_char_t type = utf8totex_from_char(&t, c);
+
+                switch (type) {
+                    case UTF8TOTEX_ASCII:
+                        FLUSH_LOOKAHEAD();
+                        _lookahead[0] = c;
+                        lookahead = _lookahead;
+                        break;
+
+                    case UTF8TOTEX_SEQUENCE:
+                    case UTF8TOTEX_SEQUENCE_T1:
+                    case UTF8TOTEX_SEQUENCE_TEXTCOMP:
+                        FLUSH_LOOKAHEAD();
+                        lookahead = t;
+                        break;
+
+                    case UTF8TOTEX_MODIFIER:
+                        if (lookahead == NULL)
+                            ERR(BAD_MODIFIER);
+
+                        /* Work around older versions of LaTeX that do not know to drop
+                         * overhead dot on an 'i' or 'j' when inserting an accent.
+                         */
+                        const char *prefix = "";
+                        if ((lookahead[0] == 'i' || lookahead[0] == 'j') &&
+                            strncmp(t, "{\\", sizeof("{\\") - 1) == 0 &&
+                            (t[2] == '"' || t[2] == '\'' || t[2] == '.' ||
+                             t[2] == '=' || t[2] == '^' || t[2] == '`' ||
+                             t[2] == '~' || t[2] == 'H' || t[2] == 'r' ||
+                             t[2] == 't' || t[2] == 'u' || t[2] == 'v'))
+                            prefix = "\\";
+
+                        if (fprintf(f, "%s%s", t, prefix) < 0)
+                            ERR(EOF);
+                        FLUSH_LOOKAHEAD();
+                        if (fputc('}', f) == EOF)
+                            ERR(EOF);
+                        break;
+
+                    case UTF8TOTEX_UNSUPPORTED:
+                    case UTF8TOTEX_INVALID:
+                        if (error != NULL)
+                            *error = type;
+                        return EOF;
+
+                    default:
+                        /* These are never returned by `utf8totex_from_char`. */
+                        assert(!"unreachable");
+                }
+                break;
+
+            } case MACRO: {
+
+                assert(bibtex);
+
+                if (length != 1 || (!isalpha(c) && c != L'{'))
+                    ERR(BAD_LITERAL);
+
+                assert(lookahead == NULL);
+                PUTC(c);
+                if (c == L'{') {
+                    state = BRACED;
+                    assert(brace_depth == 0);
+                    brace_depth = 1;
+                }
+
+                break;
+
+            } case BRACED: {
+
+                assert(bibtex);
+                assert(brace_depth > 0);
+
                 if (length != 1)
-                    ERR(INVALID); /* XXX */
-                FLUSH_LOOKAHEAD();
-                if (fputc(c, f) == EOF)
-                    ERR(EOF);
+                    ERR(BAD_LITERAL);
+
+                assert(lookahead == NULL);
+                PUTC(c);
                 if (c == L'{') {
                     brace_depth++;
-                } else if (c == '}') {
-                    assert(brace_depth > 0);
+                } else if (c == L'}') {
                     brace_depth--;
+                    if (brace_depth == 0)
+                        state = IDLE;
                 }
-                s++;
-                continue;
+
+                break;
+
+            } case MATH: {
+
+                assert(bibtex);
+
+                if (length != 1)
+                    ERR(BAD_LITERAL);
+
+                assert(lookahead == NULL);
+                PUTC(c);
+                if (c == L'$')
+                    state = IDLE;
+
+                break;
             }
-        }
 
-        const char *t;
-        utf8totex_char_t type = utf8totex_from_char(&t, c);
-
-        switch (type) {
-            case UTF8TOTEX_ASCII:
-                FLUSH_LOOKAHEAD();
-                _lookahead[0] = c;
-                lookahead = _lookahead;
-                break;
-
-            case UTF8TOTEX_SEQUENCE:
-            case UTF8TOTEX_SEQUENCE_T1:
-            case UTF8TOTEX_SEQUENCE_TEXTCOMP:
-                FLUSH_LOOKAHEAD();
-                lookahead = t;
-                break;
-
-            case UTF8TOTEX_MODIFIER:
-                if (lookahead == NULL)
-                    ERR(BAD_MODIFIER);
-
-                /* Work around older versions of LaTeX that do not know to drop
-                 * overhead dot on an 'i' or 'j' when inserting an accent.
-                 */
-                const char *prefix = "";
-                if ((lookahead[0] == 'i' || lookahead[0] == 'j') &&
-                    strncmp(t, "{\\", sizeof("{\\") - 1) == 0 &&
-                    (t[2] == '"' || t[2] == '\'' || t[2] == '.' ||
-                     t[2] == '=' || t[2] == '^' || t[2] == '`' ||
-                     t[2] == '~' || t[2] == 'H' || t[2] == 'r' ||
-                     t[2] == 't' || t[2] == 'u' || t[2] == 'v'))
-                    prefix = "\\";
-
-                if (fprintf(f, "%s%s", t, prefix) < 0)
-                    ERR(EOF);
-                FLUSH_LOOKAHEAD();
-                if (fputc('}', f) == EOF)
-                    ERR(EOF);
-                break;
-
-            case UTF8TOTEX_UNSUPPORTED:
-            case UTF8TOTEX_INVALID:
-                if (error != NULL)
-                    *error = type;
-                return EOF;
-
-            case UTF8TOTEX_EOF:
-            case UTF8TOTEX_BAD_MODIFIER:
-                /* These are never returned by `utf8totex_from_char`. */
-                assert(!"unreachable");
         }
 
         s += length;
